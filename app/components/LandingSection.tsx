b5958d121955dbda724163ec63992e3998d7c936
Chamoda Kavishka <142435114+Chamodakavi@users.noreply.github.com>
2025-01-12 19:49:27 +0530

    drawer done

--- app/components/LandingSection.tsx
+++ app/components/LandingSection.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Container, Grid, GridItem, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -14,6 +15,26 @@ function LandingSection() {
     width="100%"
     height="90vh"
   >
+    <Container pt={20}>
+    <Grid templateColumns={'repeat(12,1fr)'}>
+      <GridItem colSpan={{base:12 ,sm:9 , md:6}}>
+        <Box bgColor={'rgba(202, 176, 176, 0.8)'}  padding={10}  borderRadius={10}>
+            
+              <Text fontSize={'4xl'} fontWeight={'bolder'} color={'red.600'}>
+                SPARKLE CAR WASH
+              </Text>
+              <Text fontSize={'4xl'} fontWeight={'bolder'} color={'black.600'}>
+                COLOMBO
+              </Text>
+              <Text fontSize={'2xl'} color={'red.800'}>
+              We are committed to providing you with a superior service experience, prioritizing cleanliness and decency in every interaction.
+              </Text>
+              <Link href={'/booking'}><Button bgColor={'red'} color={'white'} mt={5}>BOOK NOW</Button></Link>
+            </Box>
+      </GridItem>
+    </Grid>
+      
+    </Container>
   </Box>
   
   )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,42 +1,150 @@
-import { Box, Button, Container, HStack, Text } from "@chakra-ui/react";
+"use client";
+
+import {
+  Box,
+  Button,
+  Container,
+  DrawerBackdrop,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerFooter,
+  DrawerRoot,
+  HStack,
+  Icon,
+  ListItem,
+  ListRoot,
+  Separator,
+  Text,
+} from "@chakra-ui/react";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+
+
+import newStyled from "@emotion/styled";
+
+const StyledListItem = newStyled(ListItem)`
+ padding-Top:30px;
+ padding-Bottom:30px;
+`;
+
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(true);
+  };
+
+  console.log(open);
+
   return (
-    <Box as={"nav"} bgColor={"white"} position="sticky" top="0" zIndex={10}>
-      <Container>
-        <HStack padding={7}>
-          <Box flex={1}>
-            <Text>
-              <Link href="/">Car Wash</Link>
-            </Text>
-          </Box>
-          <Box>
-            <HStack>
+    <>
+      <Box as={"nav"} bgColor={"white"} position="sticky" top="0" zIndex={10}>
+        <Container>
+          <HStack padding={7}>
+            <Box flex={1}>
+              <Text>
+                <Link href="/">Car Wash</Link>
+              </Text>
+            </Box>
+            <Box>
+              <HStack display={{ mdDown: "none", md: "flex" }}>
+                <Link href="/#ourservices">
+                  <Text>Our Services</Text>
+                </Link>
+                <Link href="/#contactus">
+                  <Text>Contact Us</Text>
+                </Link>
+                <Link href="/booking">
+                  <Button
+                    as={"button"}
+                    border={"1px solid"}
+                    padding={1}
+                    borderRadius={3}
+                    bgColor={"red"}
+                    color={"white"}
+                  >
+                    Book Now
+                  </Button>
+                </Link>
+              </HStack>
+              <Icon
+                onClick={toggleDrawer}
+                display={{ md: "none", mdDown: "block" }}
+                fontSize={30}
+              >
+                <IoMdMenu />
+              </Icon>
+            </Box>
+          </HStack>
+        </Container>
+      </Box>
+
+      {/* ------------------------------- Drawer-------------------------------------------- */}
+
+      <DrawerRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
+        <DrawerBackdrop />
+
+        <DrawerContent
+          width="50%"
+          height={"100vh"}
+          position={"fixed"}
+          right={0}
+          top={0}
+          bgColor={"white"}
+          color={"black"}
+        >
+          <ListRoot listStyle={"none"} fontSize={"md"} mt={"50px"} pl={10}>
+            <StyledListItem>
+              <Link href="/">Home</Link>
+            </StyledListItem>
+            <Separator width={'90%'} />
+            <StyledListItem>
               <Link href="/#ourservices">
                 <Text>Our Services</Text>
               </Link>
+            </StyledListItem>
+            <Separator width={'90%'} />
+            <StyledListItem>
               <Link href="/#contactus">
                 <Text>Contact Us</Text>
               </Link>
-              <Link href="/#contactus">
-                <Button
-                  as={"button"}
-                  border={"1px solid"}
-                  padding={1}
-                  borderRadius={3}
-                  bgColor={"red"}
-                  color={"white"}
-                >
-                  Book Now
-                </Button>
+            </StyledListItem>
+            <Separator width={'90%'}/>
+            <StyledListItem>
+              <Link href="/#aboutUs">
+                <Text>About Us</Text>
               </Link>
-            </HStack>
-          </Box>
-        </HStack>
-      </Container>
-    </Box>
+            </StyledListItem>
+            <Separator width={'90%'} />
+            <StyledListItem>
+              <Link href="/booking">
+                    <Button
+                      as={"button"}
+                      border={"1px solid"}
+                      padding={1}
+                      borderRadius={3}
+                      bgColor={"red"}
+                      color={"white"}
+                      width={'70%'}
+                      fontSize={'md'}
+                    >
+                      Book Now
+                    </Button>
+                  </Link>
+            </StyledListItem>
+            <Separator width={'90%'} />
+          </ListRoot>
+          <DrawerFooter position={'relative'} top={350}>
+            <Text fontStyle={'italic'} fontSize={'xl'}>Sparkle Car Wash</Text>
+          </DrawerFooter>
+
+          <DrawerCloseTrigger />
+        </DrawerContent>
+      </DrawerRoot>
+    </>
   );
 }
 
